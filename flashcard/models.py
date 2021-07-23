b90cb1020e6d8999906d843f66fc59131d6b1a0d
careymwarabu <careyakmwarabu@gmail.com>
2021-07-23 10:22:25 +0300

    update my work

--- flashcard/models.py
+++ flashcard/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.utils import timezone
+import cloudinary
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    profile_photo = cloudinary.models.CloudinaryField('image',null=True, blank=True)
+    email = models.EmailField(max_length=200)
+
+    def __str__(self):
+        return self.user.username
+
+    @receiver(post_save, sender=User)
+    def update_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+        instance.profile.save()
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+class Flashcard(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.TextField(max_length=300)
+    folder= models.ForeignKey(Folder, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(default=timezone.now())
+    date_updated = models.DateTimeField(default=timezone.now())
+
+class Folder(models.Model):
+    title = models.CharField(max_length=150)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
--- project/settings.py
+++ project/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-d4jh+99l6v16#xb!%jj&icoi58u68-@w%6l#o2!myjy-*k7+$e'
+SECRET_KEY ='django-insecure-d4jh+99l6v16#xb!%jj&icoi58u68-@w%6l#o2!myjy-*k7+$e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
+DEBUG = True
 
-# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -88,9 +88,9 @@ if config('MODE')=="dev":
    DATABASES = {
        'default': {
            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-           'NAME': config('DB_NAME'),
-           'USER': config('DB_USER'),
-           'PASSWORD': config('DB_PASSWORD'),
+           'NAME': 'flashcards',
+           'USER': 'carey',
+           'PASSWORD': 'carey',
            'HOST': config('DB_HOST'),
            'PORT': '',
        }
